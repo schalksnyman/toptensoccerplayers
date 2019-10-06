e0e5d6ccb007c0b7fe0af9723668285f2bf3d6a1
schalkinator <schalk.snyman@adaptit.tech>
2019-10-06 11:15:08 +0200

    Progress

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
-base_url = "https://www.ranker.com/list/best-current-soccer-players/ranker-sports"
+base_url = "https://naijaquest.com/best-football-players-in-the-world"
 
 # Send get HTTP request
 page = requests.get(base_url)
@@ -15,4 +15,21 @@ if page.status_code == requests.codes.ok:
   # Get the whole webpage in beautiful soup format
   bs = BeautifulSoup(page.text, 'lxml')
 
+  # Find the list of players
+  list_of_all_players = bs.find('div', class_='entry-content').find_all('ul')
+  top_ten_players = list_of_all_players[:10]
+  #print(top_ten_players)
 
+  # Hold the scraped data
+  data = {
+    'Rank': [],
+    'Country': [],
+    'Player': [],
+    'Team': []
+  }
+
+player = top_ten_players[0]
+rank = player.find('strong').text.split('.')[0]
+country = player.find('strong').text.split('–')[-1].strip()
+
+print(country)
