0969df526e9a8cf1ba61e334e7c3303cc33b53f6
schalkinator <schalk.snyman@adaptit.tech>
2019-10-06 15:02:21 +0200

    Basic project completed

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import pandas as pd
 
 base_url = "https://naijaquest.com/best-football-players-in-the-world"
 
 # Send get HTTP request
 page = requests.get(base_url)
-print(type(page))
-print(page.status_code)
+# print(type(page))
+# print(page.status_code)
 
 # Verify we had a successful get request webpage call
 if page.status_code == requests.codes.ok:
@@ -22,14 +23,59 @@ if page.status_code == requests.codes.ok:
 
   # Hold the scraped data
   data = {
-    'Rank': [],
     'Country': [],
-    'Player': [],
+    'Name': [],
     'Team': []
   }
 
-player = top_ten_players[0]
-rank = player.find('strong').text.split('.')[0]
-country = player.find('strong').text.split('–')[-1].strip()
+  # Scrape all ten players in the top ten list
+  for player in top_ten_players:
+    tidy_player_text_list = player.find('strong').text.replace('-','').replace('–','').replace('—','').split()
+    # print(tidy_player_text_list)
+    # rank = tidy_player_text_list[0].split('.')[0]
+    # if rank:
+    #   data['Rank'].append(rank)
+    # else:
+    #   data['Rank'].append('none')
+    # print(rank)
+    country = tidy_player_text_list[-1]
+    # Fix bad data
+    if country == "Englan":
+      country = "England"
+    if country:
+      data['Country'].append(country)
+    else:
+      data['Country'].append('none')
+    # print(country)
+    if tidy_player_text_list[2].lower() == "van" and tidy_player_text_list[3].lower() == "der":
+      name = tidy_player_text_list[1] + " " + tidy_player_text_list[2] + " " + tidy_player_text_list[3] + " " + tidy_player_text_list[4]
+    elif tidy_player_text_list[2].lower() == "van" and tidy_player_text_list[3].lower() != "der":
+      name = tidy_player_text_list[1] + " " + tidy_player_text_list[2] + " " + tidy_player_text_list[3]
+    else:
+      name = tidy_player_text_list[1] + " " + tidy_player_text_list[2]
+    if name:
+      data['Name'].append(name)
+    else:
+      data['Name'].append('none')
+    # print(name)
+    team = tidy_player_text_list[3]
+    if tidy_player_text_list[3] not in name and tidy_player_text_list[3] not in country:
+      team = tidy_player_text_list[3]
+      if tidy_player_text_list[4] not in name and tidy_player_text_list[4] not in country:
+        team = team + " " + tidy_player_text_list[4]
+        if tidy_player_text_list[5] not in name and tidy_player_text_list[5] not in country:
+          team = team + " " + tidy_player_text_list[5]
+    else:
+      team = tidy_player_text_list[4]
+      if tidy_player_text_list[5] not in name and tidy_player_text_list[5] not in country:
+        team = team + " " + tidy_player_text_list[5]
+    if team:
+      data['Team'].append(team)
+    else:
+      data['Team'].append('none')
+    # print(team)
 
-print(country)
+  table = pd.DataFrame(data, columns=['Country', 'Name', 'Team'])
+  table.index = table.index + 1
+  print(table)
+  table.to_csv('players_of_the_year.csv', sep=',', index=False, encoding='utf-8')
